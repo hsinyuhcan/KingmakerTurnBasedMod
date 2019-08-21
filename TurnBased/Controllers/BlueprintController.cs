76ad54034e6ab651dc658e0500e168139dbdba85
Hsinyu Chan <hsinyuhcan@gmail.com>
2019-08-22 07:15:52 +0800

    Optimize BlueprintController

--- TurnBased/Controllers/BlueprintController.cs
+++ TurnBased/Controllers/BlueprintController.cs
@@ -16,49 +16,51 @@ namespace TurnBased.Controllers
     {
         // ChargeAbility
         // SwiftBlowImprovedChargeAbility
-        public BlueprintModifier<bool> ActionTypeOfCharge = new BlueprintModifier<bool>(
+        public BlueprintModifier<BlueprintAbility, bool> ActionTypeOfCharge = new BlueprintModifier<BlueprintAbility, bool>(
             () => FixActionTypeOfCharge,
             new string[] { "c78506dd0e14f7c45a599990e4e65038", "d4b4757660cb66e4fbf376a43f1ffb13" },
-            true,
-            (lib, guid) => lib.Get<BlueprintAbility>(guid).IsFullRoundAction,
-            (lib, guid, value) => lib.Get<BlueprintAbility>(guid).SetIsFullRoundAction(value));
+            (blueprint) => blueprint.IsFullRoundAction,
+            (blueprint, value) => blueprint.SetIsFullRoundAction(value),
+            true);
 
         // OverrunAbility
         // ChargeAbilityLanternKingStar
         // FlyTrampleTest
-        public BlueprintModifier<bool> ActionTypeOfOverrun = new BlueprintModifier<bool>(
+        public BlueprintModifier<BlueprintAbility, bool> ActionTypeOfOverrun = new BlueprintModifier<BlueprintAbility, bool>(
             () => FixActionTypeOfOverrun,
             new string[] { "1a3b471ecea51f7439a946b23577fd70", "49b8bf9a35ecbd24482ee416cd7557b8", "f0b622ab2d18ef7439feb8aa5680d6e5" },
-            true,
-            (lib, guid) => lib.Get<BlueprintAbility>(guid).IsFullRoundAction,
-            (lib, guid, value) => lib.Get<BlueprintAbility>(guid).SetIsFullRoundAction(value));
+            (blueprint) => blueprint.IsFullRoundAction,
+            (blueprint, value) => blueprint.SetIsFullRoundAction(value),
+            true);
 
         // VitalStrikeAbility
         // VitalStrikeAbilityImproved
         // VitalStrikeAbilityGreater
-        public BlueprintModifier<bool> ActionTypeOfVitalStrike = new BlueprintModifier<bool>(
+        public BlueprintModifier<BlueprintAbility, bool> ActionTypeOfVitalStrike = new BlueprintModifier<BlueprintAbility, bool>(
             () => FixActionTypeOfVitalStrike,
             new string[] { "efc60c91b8e64f244b95c66b270dbd7c", "c714cd636700ac24a91ca3df43326b00", "11f971b6453f74d4594c538e3c88d499" },
-            false,
-            (lib, guid) => lib.Get<BlueprintAbility>(guid).IsFullRoundAction,
-            (lib, guid, value) => lib.Get<BlueprintAbility>(guid).SetIsFullRoundAction(value));
+            (blueprint) => blueprint.IsFullRoundAction,
+            (blueprint, value) => blueprint.SetIsFullRoundAction(value),
+            false);
 
         // TristianAngelAbility
-        public BlueprintModifier<UnitCommand.CommandType> ActionTypeOfAngelicForm = new BlueprintModifier<UnitCommand.CommandType>(
-            () => FixActionTypeOfAngelicForm,
-            new string[] { "83e91b42102fdf04a98e86a0d515cd60" },
-            UnitCommand.CommandType.Move,
-            (lib, guid) => lib.Get<BlueprintActivatableAbility>(guid).ActivateWithUnitCommandType,
-            (lib, guid, value) => lib.Get<BlueprintActivatableAbility>(guid).SetActivateWithUnitCommand(value));
+        public BlueprintModifier<BlueprintActivatableAbility, UnitCommand.CommandType> ActionTypeOfAngelicForm
+            = new BlueprintModifier<BlueprintActivatableAbility, UnitCommand.CommandType>(
+                () => FixActionTypeOfAngelicForm,
+                new string[] { "83e91b42102fdf04a98e86a0d515cd60" },
+                (blueprint) => blueprint.ActivateWithUnitCommandType,
+                (blueprint, value) => blueprint.SetActivateWithUnitCommand(value),
+                UnitCommand.CommandType.Move);
 
         // InspireGreatnessToggleAbility
         // InspireHeroicsToggleAbility
-        public BlueprintModifier<bool> AbilityDeactivateIfCombatEnded = new BlueprintModifier<bool>(
-            () => FixAbilityNotAutoDeactivateIfCombatEnded,
-            new string[] { "be36959e44ac33641ba9e0204f3d227b", "a4ce06371f09f504fa86fcf6d0e021e4" },
-            true,
-            (lib, guid) => lib.Get<BlueprintActivatableAbility>(guid).DeactivateIfCombatEnded,
-            (lib, guid, value) => lib.Get<BlueprintActivatableAbility>(guid).DeactivateIfCombatEnded = value);
+        public BlueprintModifier<BlueprintActivatableAbility, bool> AbilityDeactivateIfCombatEnded 
+            = new BlueprintModifier<BlueprintActivatableAbility, bool>(
+                () => FixAbilityNotAutoDeactivateIfCombatEnded,
+                new string[] { "be36959e44ac33641ba9e0204f3d227b", "a4ce06371f09f504fa86fcf6d0e021e4" },
+                (blueprint) => blueprint.DeactivateIfCombatEnded,
+                (blueprint, value) => blueprint.DeactivateIfCombatEnded = value,
+                true);
 
         public void Update(bool modify = true)
         {
@@ -71,36 +73,54 @@ namespace TurnBased.Controllers
             AbilityDeactivateIfCombatEnded.Update(modify);
         }
 
-        public class BlueprintModifier<TValue>
+        public class BlueprintModifier<TBlueprint, TValue> where TBlueprint : BlueprintScriptableObject
         {
             private readonly Func<bool> _option;
-            private readonly string[] _assetGuid;
+            private string[] _assetGuid;
+            private readonly Func<TBlueprint, TValue> _getter;
+            private readonly Action<TBlueprint, TValue> _setter;
             private readonly TValue _value;
-            private readonly Func<LibraryScriptableObject, string, TValue> _getter;
-            private readonly Action<LibraryScriptableObject, string, TValue> _setter;
+            private TBlueprint[] _blueprints;
             private TValue[] _backup;
 
-            public BlueprintModifier(Func<bool> option, string[] assetGuid, TValue value, 
-                Func<LibraryScriptableObject, string, TValue> getter, Action<LibraryScriptableObject, string, TValue> setter)
+            public BlueprintModifier(Func<bool> option, string[] assetGuid, 
+                Func<TBlueprint, TValue> getter, Action<TBlueprint, TValue> setter, TValue value)
             {
                 _option = option;
                 _assetGuid = assetGuid;
-                _value = value;
                 _getter = getter;
                 _setter = setter;
+                _value = value;
+            }
+
+            private void TryGetBlueprints()
+            {
+                if (_assetGuid == null)
+                    return;
+
+                LibraryScriptableObject library = typeof(ResourcesLibrary).GetFieldValue<LibraryScriptableObject>("s_LibraryObject");
+                if (library != null && library.GetInitialized())
+                {
+                    try
+                    {
+                        _blueprints = _assetGuid.Select(guid => library.Get<TBlueprint>(guid)).ToArray();
+                        _backup = _blueprints.Select(blueprint => _getter(blueprint)).ToArray();
+                        _assetGuid = null;
+                    }
+                    catch (Exception e)
+                    {
+                        Mod.Error(e);
+                    }
+                }
             }
 
             public void Update(bool modify = true)
             {
-                LibraryScriptableObject library = typeof(ResourcesLibrary).GetFieldValue<LibraryScriptableObject>("s_LibraryObject");
-                if (library != null && library.GetInitialized())
-                {
-                    if (_backup == null)
-                        _backup = _assetGuid.Select(guid => _getter(library, guid)).ToArray();
+                TryGetBlueprints();
 
-                    for (int i = 0; i < _assetGuid.Length; i++)
-                        _setter(library, _assetGuid[i], (modify && _option()) ? _value : _backup[i]);
-                } 
+                if (_blueprints != null && _backup != null)
+                    for (int i = 0; i < _blueprints.Length; i++)
+                        _setter(_blueprints[i], (modify && _option()) ? _value : _backup[i]);
             }
         }
     }
